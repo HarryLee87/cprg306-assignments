e559b42640904e3aaaf777f246e86be46e5cbe7b
HarryLee87 <haewon.harry.lee@gmail.com>
2023-09-14 14:34:52 -0600

    Updating header in Main page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import MyComp from './mycomp'
 import StudentInfo from './StudentInfo'
 import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
       <Link href="/week2">Week2</Link>
     </main>
