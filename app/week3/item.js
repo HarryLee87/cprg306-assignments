63a3da47a3987463cfc9f3883466884721231cec
HarryLee87 <haewon.harry.lee@gmail.com>
2023-09-21 13:47:19 -0600

    Updated Style and output

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,9 +1,8 @@
 export default function Item({name, quantity, category}) {
     return(
-        <div className="p-6 m-1 border border-red-50o bg-stone-800 ">
-            <h2 className = "text-2xl font-bold text-justified text-red-100">{name}</h2>
-            <p className = "text-xl text-orange-100">Qty: {quantity}</p>
-            <p className = "text-xl text-orange-100">Cat: {category}</p>
+        <div className="p-6 m-2 border border-red-500 rounded-lg bg-stone-700 w-auto">
+            <h2 className = "text-2xl font-bold text-justified text-yellow-300">{name}</h2>
+            <p className = "text-xl text-white">Buy {quantity} in {category}</p>
         </div>
     );
 }
--- app/week3/page.js
+++ app/week3/page.js
@@ -5,7 +5,7 @@ import ItemList from "./item-list";
 export default function Page() {
     return(
         <main>
-            <h1 className="text-5xl font-semibold text-justified text-justified mb-5">
+            <h1 className="text-5xl font-semibold text-justified text-justified m-5">
                 Shopping List
             </h1>
             <ul>
